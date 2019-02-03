972cf26f6c72413f3620fdefc114ef272c4b68e9
Phongtlam <Phongtlam@gmail.com>
2019-02-03 12:33:55 -0800

    add disclaimer

--- src/containers/About.js
+++ src/containers/About.js
@@ -503,6 +503,16 @@ class About extends React.PureComponent {
             ))}
           </ul>
         </div>
+
+        <br />
+        <div className="App-About-disclaimer">
+          <h3>DISCLAIMER</h3>
+          <p>
+            All product and company names are trademarks™ or registered®
+            trademarks of their respective holders. Use of them does not imply
+            any affiliation with or endorsement by them.
+          </p>
+        </div>
       </div>
     );
   }
